c981749b37e73bf8ee7710d0dccacb34b9df1f24
Федор Шарапов <sharapovfedor@yandex.ru>
2022-09-30 17:49:58 +0300

    обобщённая функция расширения для поиска максимального элемента коллекции.

---
+++ src/Delegates.Events/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Delegates.Events
+{
+    public static class EnumerableExtensions
+    {
+        public static T? GetMax<T>(this IEnumerable<T> source, Func<T, float> getParameter) where T : class
+        {
+            if (source is null || !source.Any())
+                throw new InvalidOperationException("Cannot compute maximum for a null or empty set.");
+            else if(getParameter is null)
+                throw new InvalidOperationException("Cannot compute maximum because delegate converting input type to number is null.");
+            
+            return source.MaxBy(x => getParameter(x));
+        }
+    }
+}
--- src/Delegates.Events/Program.cs
+++ src/Delegates.Events/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Delegates.Events
+﻿using System.Diagnostics;
+
+namespace Delegates.Events
 {
     internal class Program
     {
+        static float GenericToFloat<T>(T element) where T : class
+        {
+            return element.GetHashCode();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Поиск максимального значения
+            var numbers = new string[] { "1,7", "1,2", "1,9", "1,3", "1,4", "1,0", "1,5", "1,1", "1,6", "1,8" };
+            var max = numbers.GetMax(x => (float)Convert.ToDouble(x));
+            Console.WriteLine($"Максимальное значение: {max}");
+
+
+
         }
     }
 }
