79482607b2615074df76c0e7448cf5290bd15f3f
Федор Шарапов <sharapovfedor@yandex.ru>
2022-09-30 15:04:18 +0300

    Инициализация

---
+++ src/Delegates.Events/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Delegates.Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
