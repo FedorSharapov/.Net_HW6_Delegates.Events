fb8f5b17011a7047ede81f06a793be9841fcace1
Федор Шарапов <sharapovfedor@yandex.ru>
2022-10-01 01:14:43 +0300

    вывод в Console коллекцию чисел

--- src/Delegates.Events/Program.cs
+++ src/Delegates.Events/Program.cs
@@ -9,6 +9,7 @@
             // Поиск максимального значения
             var numbers = new string[] { "1,7", "1,2", "1,9", "1,3", "1,4", "1,0", "1,5", "1,1", "1,6", "1,8" };
             var max = numbers.GetMax(x => (float)Convert.ToDouble(x));
+            ConsoleHelper.WriteLine($"Коллекция чисел: [{string.Join("; ", numbers)}]");
             ConsoleHelper.WriteLine($"Максимальное значение: [{max}]\r\n");
 
             // Обнаружение файлов в каталоге
